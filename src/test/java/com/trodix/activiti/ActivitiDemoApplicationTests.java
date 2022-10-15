643dcad298dfb01450c44abe1921367645855b89
Sébastien Vallet <sebastien.vallet89@gmail.com>
2022-10-15 17:55:57 +0200

    initial commit

---
+++ src/test/java/com/trodix/activiti/ActivitiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trodix.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
